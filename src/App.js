f45174cc663ab1f773f75a573de36571bc86eee9
Zivorinashe <89390399+Zivorinashe@users.noreply.github.com>
2021-09-20 15:39:26 +0200

    mfakose

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 // import './App.css';
 import React from 'react'
 
-// import {
-// BrowserRouter as Router,
-// Route,
-// Redirect,
-// Switch
-//  } from 'react-router-dom';
+import {
+BrowserRouter as Router,
+Route,
+Redirect,
+Switch
+ } from 'react-router-dom';
 
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
@@ -15,27 +15,27 @@ import Service from './pages/Services/Services';
 import Testimonials from './pages/Testimonials/Testimonials';
 
 const App = () => {
-  // return (
-    // <Router>
-    //   <main>
-    //    <Switch>
-    //    <Route path="/" exact>
-    //       <Home/>
-    //     </Route>
-    //     <Route path="/" exact>
-    //       <About/>
-    //     </Route>
-    //     <Route path="/" exact>
-    //       <Service/>
-    //     </Route>
-    //     <Route path="/" exact>
-    //       <Testimonials/>
-    //     </Route>
-    //     <Redirect to = "/"/>
-    //    </Switch>
-    //   </main>
-    // </Router>
-  // );
+  return (
+    <Router>
+      <main>
+       <Switch>
+       <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/" exact>
+          <About/>
+        </Route>
+        <Route path="/" exact>
+          <Service/>
+        </Route>
+        <Route path="/" exact>
+          <Testimonials/>
+        </Route>
+        <Redirect to = "/"/>
+       </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Events/Events.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Events = () =>{
+    return(
+        <div className='container'>
+            <h1 className='text-center' > Events</h1>
+        </div>
+    )
+}
+
+export default Events;
