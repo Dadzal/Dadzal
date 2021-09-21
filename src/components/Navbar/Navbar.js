4d1b957f4a6087ef5c8f21d2c0aa9e567ba6be9c
Zivorinashe <89390399+Zivorinashe@users.noreply.github.com>
2021-09-21 12:18:00 +0200

    Mfakose

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import Navbar from './Navbar.css';
+// import Navbar from './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return(
        <nav className="navbar navbar-expand-lg navbar-mainbg">
            <NavLink className='navbar-brand navbar-logo' to="/" exact>
-                Web Solution
+                Mufakose
            </NavLink>
+
+           <button
+           className="navbar-toggler"
+           type="button"
+           data-toggle="collapse"
+           data-target="#navbarSupportedContent"
+           aria-controls="navbarSupportedContent"
+           aria-expanded="false"
+           aria-label="Toggle navigation" 
+           >
+         <i className="fas fa-bars text-white"></i>
+           </button>
+
        </nav>
     )
 }
