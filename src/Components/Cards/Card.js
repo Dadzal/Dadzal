6cfaf01d7de3696948c3a5761d167c7261fa2245
TeeynohDale <dale@uncommon.org>
2021-10-11 11:03:45 +0200

    CardForm

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import PropTypes from 'prop-types'
 
- function Card() {
+ function Card(props) {
 
-    const department = "WEB DEVELOPMENT";
-    const venue = "MUFAKOSE HUB";
-    const details = "monthly goals and aob";
-    const time = `09:00`
+    const {department = "WEB DEVELOPMENT", venue = "MUFAKOSE HUB",details = "monthly goals and aob", time = `09:00`, } = props
+   
     
     return (
         <div className="col-md-4 ">
             <div className="card ">
-                <div class="card-body">
-                    <h5 class="card-title">
+                <div className="card-body">
+                    <h5 className="card-title">
                         {department}
                     </h5>
-                    <p class="card-text">
+                    <p className="card-text">
                         {details}
                     </p>
                     <div className="card-footer">
@@ -29,6 +27,11 @@ import React from 'react';
         </div>
     )
 }
-
+Card.propTypes = {
+	department: PropTypes.string,
+	venue: PropTypes.string,
+    time: PropTypes.string,
+	details: PropTypes.string
+};
 
 export default Card;
