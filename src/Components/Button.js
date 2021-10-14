c9ebf93c353bda4b18166f704bb11fbba66e8ea4
Zivorinashe <89390399+Zivorinashe@users.noreply.github.com>
2021-10-14 10:48:16 +0200

    mfakose

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './Button.css';
-import { Link, NavLink } from 'react-router-dom';
+// import React from 'react';
+// import './Button.css';
+// import { Link, NavLink } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline']
-const SIZE = ['btn--medium', 'btn--large']
+// const STYLES = ['btn--primary', 'btn--outline']
+// const SIZE = ['btn--medium', 'btn--large']
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize
-}) =>{
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle:STYLES[0 ]
-    return(
-       <Link>
-       <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-       onClick={onClick}
-       type={onClick}
-       >
-           {children}
-       </button>
-       </Link>
-    )
-};
+// export const Button = ({children, type, onClick, buttonStyle, buttonSize
+// }) =>{
+//     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle:STYLES[0 ]
+//     return(
+//        <Link>
+//        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+//        onClick={onClick}
+//        type={onClick}
+//        >
+//            {children}
+//        </button>
+//        </Link>
+//     )
+// };
